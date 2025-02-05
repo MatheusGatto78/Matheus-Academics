38e061464cb542bd7cff692ed83c7c12421b49e0
MatheusGatto78 <matheusviniciusgatto@gmail.com>
2025-02-05 11:08:15 -0300

    atividade CRUD

--- back_end/Atividades Flask/LIVRARIA(atividadePostmen)/app.py
+++ back_end/Atividades Flask/LIVRARIA(atividadePostmen)/app.py
@@ -1,28 +1,28 @@
-from flask import Flask 
+from flask import Flask, request
 
 app = Flask(__name__) 
 
 livro = {
-    "1984": 5,
-    "Dom Casmurro": 18,
-    "O Senhor dos Anéis": 34,
-    "Harry Potter e a Pedra Filosofal": 3,
-    "A Revolução dos Bichos": 8
+    "1": "1984",
+    "2": "Dom Casmurro",
+    "3": "O Senhor dos Anéis",
+    "4": "Harry Potter e a Pedra Filosofal",
+    "5": "A Revolução dos Bichos"
 }
 
 autor = {
-    "George Orwell": 46,  
-    "Machado de Assis": 61, 
-    "J.R.R. Tolkien": 81,  
-    "J.K. Rowling": 58  
+    "1": "George Orwell",  
+    "2": "Machado de Assis", 
+    "3": "J.R.R. Tolkien",  
+    "4": "J.K. Rowling" 
 }
 
 cliente = {
-    "Carlos": 3,
-    "Ana": 5,
-    "Pedro": 2,
-    "Mariana": 4,
-    "Lucas": 1
+    "1": "Carlos",
+    "2": "Ana",
+    "3": "Pedro",
+    "4": "Mariana",
+    "5": "Lucas"
 }
 
 @app.route("/")
@@ -36,17 +36,31 @@ def homepage():
     <a href="/clientes">Ir para a página de Clientes</a>
     '''
 
-@app.route("/livros")  
+@app.route("/livros", methods=["get"])  
 def livros():
     return livro
 
-@app.route("/autores")  
+@app.route("/autores", methods=["get"])  
 def autores():
     return autor
 
-@app.route("/clientes")  
-def cliente():
-    return autor
+@app.route("/clientes", methods=["get"])  
+def clientes():
+    return cliente
+
+@app.route("/delete_livro/<string:id>", methods=["delete"])
+def delete_livro(id):
+    print(livro[id])
+    print(f"\n\n-->{id}\n\n")
+    del livro[id]
+    return livro
+
+@app.route("/add_livro", methods=["post"])
+def add_livro():
+    data = request.get_json()
+    print(data)
+    livro[f"{len(livro) + 1}"] = data["Titulo"]
+    return livro
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
